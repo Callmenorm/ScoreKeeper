f0033c71485ad95faa013dec34324c61bca15f5c
Michael Anderson <michael.anderson@campingworld.com>
2016-03-18 23:34:17 -0600

    First of ScoreKeeper

---
+++ src/incrementControl.js
@@ -0,0 +1,10 @@
+/**
+ * Created by michael.anderson on 3/18/16.
+ */
+import React, {View} from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const IncrementControl = () => <Icon name='plus' backgroundColor='blue' color='black' size={20} />;
+
+export default IncrementControl;
