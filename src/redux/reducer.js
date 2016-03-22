59a5c9055d2137c7a54b5f0addb7c2731c7b1395
Michael Anderson <michael.anderson@campingworld.com>
2016-03-21 22:24:20 -0600

    Added reducer functions for adding/removing scoreboxes

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -8,6 +8,10 @@ const defaultState = {
     }]
 };
 
+const defaultScore = {
+    score: 0
+};
+
 const scores = (state = defaultState, action) => {
     switch (action.type) {
         case 'ADD':
@@ -19,6 +23,26 @@ const scores = (state = defaultState, action) => {
                         Object.assign({}, state.scores[action.index], {
                             score: state.scores[action.index] + action.scoreDelta
                         }),
+                        ...state.scores.slice(action.index + 1)
+                    ]
+                });
+            }
+            return state;
+        case 'ADD_SCOREBOX':
+            const startingScore = action.score || 0;
+            return Object.assign({}, state, {
+                scores: [
+                    ...state.scores,
+                    Object.assign({}, defaultScore, {
+                        score: startingScore
+                    })
+                ]
+            });
+        case 'REMOVE_SCOREBOX':
+            if (action.index) {
+                return Object.assign({}, state, {
+                    scores: [
+                        ...state.scores.slice(0, action.index),
                         ...state.scores.slice(action.index + 1)
                     ]
                 });
